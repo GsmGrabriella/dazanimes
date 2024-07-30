9a05517be2de437618bd12585e2e01064ecc472b
Kayk Mascarenhas <kaykdsg@gmail.com>
2024-07-29 21:51:12 -0300

    feat: register page

--- front/urls.py
+++ front/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from front.views import login
+from front.views import login, register
 
 urlpatterns = [
   path('login', login, name='login'),
+  path('register', register, name='register'),
 
 ]
--- front/views.py
+++ front/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
 
 def login(request):
-    return render(request, 'front/auth/login.html')
+    if request.method == 'GET':
+        return render(request, 'front/auth/login.html')
+    elif request.method == 'POST':
+        return
+    else:
+        return render(request, 'front/notfound.html')
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'front/auth/register.html')
+    elif request.method == 'POST':
+        username = str(request.POST.get('username'))
+        email = str(request.POST.get('email'))
+        password = str(request.POST.get('password'))
+        password_confirm = str(request.POST.get('password_confirm'))
+
+        data = [
+            username,
+            email,
+            password,
+            password_confirm
+        ]
+
+        return render(request, 'front/auth/data.html', {"data":data})
+    else:
+        return render(request, 'front/notfound.html')
+    
