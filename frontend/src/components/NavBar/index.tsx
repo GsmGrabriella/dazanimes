201067f20b4d159af4cf086dd872257d505f0022
Kayk Mascarenhas <kaykdsg@gmail.com>
2024-11-09 16:24:43 -0300

    refactor: using context on navbar user

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,7 +1,8 @@
 // importa as dependências necessárias
 import React from 'react'; // importa o React
-import { useState, useEffect } from 'react' // importa o useState
+import { useState, useEffect, useContext } from 'react' // importa o useState
 import { useLocation } from "react-router-dom"; // importa o useNavigate
+import { UserContext } from '../../contexts/UserContext'; // importa o contexto UserContext
 import api from '../../util/api'; // importa a instância do axios
 import logo from '../../assets/logo.png'; // importa a logo
 
@@ -10,15 +11,8 @@ import './styles.css'; // importa o arquivo de estilos
 // define o componente NavBar
 const NavBar: React.FC = () => {
   // cria um estado para armazenar o usuário
-  const [user, setUser] = useState<any>(null);
-
-  // define o efeito colateral para buscar o usuário no localStorage
-  useEffect(() => {
-    const user = localStorage.getItem('user');
-    if (user) {
-      setUser(JSON.parse(user));
-    }
-  }, []);
+  const userContext = useContext(UserContext);
+  const user = userContext?.user;
 
   const location = useLocation();
 
